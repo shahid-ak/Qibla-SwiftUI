4d1903ff233f7e1033feddf741d6e434f4e3547b
shahid.khader <shahidabdlk@gmail.com>
2024-03-25 17:35:58 +0530

    Initial Commit

---
+++ Qibla/QiblaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiblaApp.swift
+//  Qibla
+//
+//  Created by shahid on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct QiblaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Qibla/QiblaCompassViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  QiblaCompassViewModel.swift
+//  Qibla
+//
+//  Created by shahid on 25/03/24.
+//
+
+import Foundation
+import MapKit
+
+class QiblaCompassViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+}
