616a9e74307b847facd55d4d884567132666fd0c
shahid.khader <shahidabdlk@gmail.com>
2024-03-28 15:34:53 +0530

    QIbla UI Integrations

--- Qibla/CompassView.swift
+++ Qibla/CompassView.swift
@@ -1,5 +1,5 @@
 //
-//  CopassView.swift
+//  CompassView.swift
 //  Qibla
 //
 //  Created by shahid on 27/03/24.
@@ -10,45 +10,77 @@ import SwiftUI
 struct CompassView: View {
     
     @ObservedObject var viewModel: QiblaViewModel
+    @State var wave = false
     
     var body: some View {
-        GeometryReader{ geo in
-            ZStack{
-                ZStack {
-                    //Top Circle
-                    VStack {
-                        Circle()
-                            .fill(Color(viewModel.mode == .ahead ? UIColor.label : UIColor.secondaryLabel))
-                            .frame(width: 16, height: 16)
-                            .offset(y: viewModel.mode == .ahead ? -4 : -8)
-                            .scaleEffect(viewModel.mode == .ahead ? 2 : 1)
-                        
-                        Spacer()
-                    }
+        
+        ZStack(){
+            ZStack {
+                //Top Circle
+                VStack {
+                    Circle()
+                        .fill(Color("transparentGrey"))
+                        .frame(width: 18, height: 18)
+                        .offset(y: -9)
+                        .opacity(viewModel.mode == .ahead ? 0 : 1)
                     
-                    //SemiCircle
-                    ZStack {
-                        Circle()
-                            .trim(from: 0.025, to: (viewModel.directionOfKabahTo360Format/360) - 0.025)
-                            .rotation(Angle(degrees: -90))
-                            .stroke(Color(UIColor.label), style: StrokeStyle(lineWidth: 16, lineCap: .round))
-                            .opacity(viewModel.mode == .ahead ? 0 : 1)
-                    }
-                    
-                    // Aroow with top circle
-                    ZStack {
-                        Circle()
-                            .frame(width: 16, height: 16)
-                            .offset(y: -geo.size.width/2)
-                            .opacity(viewModel.mode == .ahead ? 0 : 1)
-                        Image(systemName: "arrow.up")
-                            .font(.system(size: 192, weight: .bold))
-                    }
-                    .rotationEffect(viewModel.directionOfKabah)
+                    Spacer()
                 }
+                
+                //SemiCircle
+                ZStack {
+                    if viewModel.mode == .right {
+                        Circle()
+                            .trim(from: 0.03, to: (viewModel.directionOfKabahTo360Format/360) - 0.03)
+                            .rotation(Angle(degrees: -90))
+                            .stroke(Color("semicircleGrey"), style: StrokeStyle(lineWidth: 16, lineCap: .round))
+                    } else if viewModel.mode == .left {
+                        Circle()
+                            .trim(from:(viewModel.directionOfKabahTo360Format/360) + 0.03, to: 0.97)
+                            .rotation(Angle(degrees: -90))
+                            .stroke(Color("semicircleGrey"), style: StrokeStyle(lineWidth: 16, lineCap: .round))
+                    }
+                }
+                
+                // Aroow with top circle
+                ZStack {
+                    ZStack{
+                        Circle().fill(.white).frame(width: viewModel.mode == .ahead ? 32 : 18, height: viewModel.mode == .ahead ? 32 : 18)
+                        if viewModel.mode == .ahead {
+                            PulsatingCircle(mode: $viewModel.mode)
+                        }
+                    }
+                    .offset(y: -312/2)
+                    Image(systemName: "arrow.up")
+                        .font(.system(size: 192, weight: .bold))
+                        .foregroundColor(.white)
+                }
+                .rotationEffect(viewModel.directionOfKabah)
             }
-            .frame(width: geo.size.width, height: geo.size.width)
-            
+            .frame(maxWidth: .infinity, maxHeight: 312)
         }
+    }
+}
+
+struct PulsatingCircle: View {
+    
+    @Binding var mode: Mode
+    @State var animate = false
+    
+    var body: some View {
+        ZStack{
+            Circle()
+                .fill(.white.opacity(0.1))
+                .frame(width: mode == .ahead ? 32 * 6 : 16 * 6, height: mode == .ahead ? 32 * 6 : 16 * 6)
+                .scaleEffect(animate ? 1 : 0)
+            Circle()
+                .fill(.white.opacity(0.2))
+                .frame(width: mode == .ahead ? 32 * 3 : 16 * 3, height: mode == .ahead ? 32 * 3 : 16 * 3)
+                .scaleEffect(animate ? 1 : 0)
+        }
+        .onAppear(){
+            animate = true
+        }
+        .animation(Animation.linear(duration: 1).repeatForever(autoreverses: true))
     }
 }
